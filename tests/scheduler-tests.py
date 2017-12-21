f3da301dc53088d92a27f5ef0406a8fc82b258c4
David Villa Alises <david.villa@gmail.com>
2017-12-21 15:29:44 +0100

    some test ideas

---
+++ tests/scheduler-tests.py
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8; mode: ruby; tab-width:4 -*-
+
+# get plan for existing service:     event "recorded sound"
+# get plan for tirivial composition: event "voice command"

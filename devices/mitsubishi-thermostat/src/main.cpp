4573987c32f18777b19f2ca0696ec296aa23c5b2
Oscar Aceña <oscaracena@gmail.com>
2018-01-11 13:54:14 +0100

    devices: working on smart-office thermostat, not finished

---
+++ devices/mitsubishi-thermostat/src/main.cpp
@@ -0,0 +1,97 @@
+// -*- mode: cpp; coding: utf-8 -*-
+
+#include <Arduino.h>
+
+#include <ArduinoOTA.h>    // force IDE to include this
+#include <ESP8266WiFi.h>   // force IDE to include this
+// #include <ESP8266mDNS.h>   // force IDE to include this
+// #include <EEPROM.h>        // force IDE to include this
+
+#include <IceC.h>
+#include <IceC/platforms/esp8266/TCPEndpoint.h>
+#include <IceC/platforms/esp8266/debug.hpp>
+#include <IceC-IoT-node.h>
+
+#include "node.h"
+
+// IceC broker and adapter
+Ice_Communicator ic;
+Ice_ObjectAdapter adapter;
+
+// servants
+IoT_Node node;
+
+// pin configuration
+#define PIN_STATUS    14    // thermostat on or off
+#define PIN_POWER     13
+#define PIN_TEMP_UP   4
+#define PIN_TEMP_DOWN 5
+
+void
+setup_pins() {
+    pinMode(PIN_STATUS, INPUT);
+    pinMode(PIN_POWER, OUTPUT);
+    pinMode(PIN_TEMP_UP, OUTPUT);
+    pinMode(PIN_TEMP_DOWN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void
+blink(byte count, byte delay_ms) {
+    for (byte i=0; i<count; i++) {
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(50);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(delay_ms);
+    }
+}
+
+void
+IoT_NodeAdminI_restart(IoT_NodeAdminPtr self) {
+    async_restart_node();
+}
+
+void
+IoT_NodeAdminI_factoryReset(IoT_NodeAdminPtr self) {
+    async_factory_reset();
+}
+
+void
+IoT_WiFiAdminI_setupWiFi(IoT_WiFiAdminPtr self,
+                         Ice_String ssid,
+                         Ice_String key) {
+    store_wifi_settings(ssid, key);
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.flush();
+    setup_pins();
+    blink(3, 100);
+
+    Serial.println("\n------\nBooting...\n");
+
+    // setup WiFi, Ice, Endpoints...
+    IceC_Storage_begin();
+    setup_wireless();
+    setup_ota();
+    Ice_initialize(&ic);
+    TCPEndpoint_init(&ic);
+
+    // setup adapter and register servants
+    Ice_Communicator_createObjectAdapterWithEndpoints
+        (&ic, "Adapter", "tcp -p 4455", &adapter);
+    Ice_ObjectAdapter_activate(&adapter);
+    IoT_Node_init(&node);
+
+    // FIXME: add interface to set these identities
+    Ice_ObjectAdapter_add(&adapter, (Ice_ObjectPtr)&node, "Node");
+
+    Serial.println("Boot done!\n");
+}
+
+void loop() {
+    handle_ota();
+    check_buttons();
+    Ice_Communicator_loopIteration(&ic);
+}
---
+++ devices/mitsubishi-thermostat/src/node.ice
@@ -0,0 +1,8 @@
+// -*- mode: c++; coding: utf-8 -*-
+
+#include <iot/node.ice>
+
+module IoT {
+    interface Node extends NodeAdmin, WiFiAdmin {
+    };
+};
